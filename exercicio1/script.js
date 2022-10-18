23555b6d5b8430c5998635deec350c942abf54f3
reydnerguimaraes <reydnerggwp@gmail.com>
2022-10-18 19:47:00 -0300

    exercicio1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,35 @@
+
+
+let nome
+let idade
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+// foi impresso porque nao atribuiu valor
+
+nome = prompt("qual seu nome ?")
+idade =Number( prompt("qual sua idade ?"))
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+//atribui valores mostrou agora o que foi atribuido
+
+console.log("olá", nome,"você tem", idade, "anos" ) 
+
+const perguntaA = "voce tem animal"
+const perguntaB = "e macho"
+const perguntaC = "e femea"
+
+const respostaA = prompt (perguntaA)
+const respostaB = prompt (perguntaB)
+const respostaC = prompt (perguntaC)
+
+console.log(perguntaA, respostaA)
+console.log(perguntaB, respostaB)
+console.log(perguntaC, respostaC)
+
+
+
+
