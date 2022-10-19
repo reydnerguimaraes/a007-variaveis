181a134fa3e2d504fa0224fe1d8ce8043f71f47f
reydnerguimaraes <reydnerggwp@gmail.com>
2022-10-18 21:29:45 -0300

    exercicio3-feito

---
+++ exercicio3/script.js
@@ -0,0 +1,53 @@
+const nome = "qual seu nome completo"
+const nascimento = "qual data de nascimento"
+let endereco = "qual seu endereço"
+const cpf = "cpf"
+const escolaridade = "escolaridade"
+const possuiCnh = "possui CNH"
+const filhos = "quantos filhos possui"
+const cargo =  "qual seu cargo atual"
+const salario = "qual salario atual"
+const comissao = "Recebe comissão? Se sim, informe a porcentagem, caso não, responda com zero"
+const anoAdmissao = "ano de admissao"
+
+const rsNome =prompt (nome)
+const rsNascimento =prompt (nascimento)
+const rsEndereco =prompt (endereco)
+const rsCpf =prompt (cpf)
+const rsEscolaridade =prompt (escolaridade)
+const rsPossuiCnh =prompt (possuiCnh)
+const rsFilhos =Number (prompt (filhos))
+const rsCargo =prompt (cargo)
+const rsSalario =Number (prompt (salario))
+const rsComissao =Number (prompt (comissao))
+const rsAnoAdmissao =Number (prompt (anoAdmissao))
+
+console.log(nome, rsNome)
+console.log(nascimento, rsNascimento)
+console.log(endereco, rsEndereco)
+console.log(cpf, rsCpf)
+console.log(escolaridade, rsEscolaridade)
+console.log(possuiCnh, rsPossuiCnh)
+console.log(filhos, rsFilhos)
+console.log(cargo, rsCargo)
+console.log(salario, rsSalario)
+console.log(comissao, rsComissao)
+console.log(anoAdmissao, rsAnoAdmissao)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
